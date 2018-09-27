6535ec229434d875db84c7b67ffb576e453eebba
dipaaapi <dipaaapi8@gmail.com>
2018-09-27 13:39:32 +0800

    front-end

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,6 +11,7 @@
     
     <!-- Bootstrap core CSS -->
     <link href="/assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/assets/css/main.css" rel="stylesheet">
     <!-- Custom fonts for this template -->
     <link href="/assets/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
@@ -19,12 +20,39 @@
     <link href="/assets/css/stylish-portfolio.min.css" rel="stylesheet">
     <link href="/assets/css/clean-blog.min.css" rel="stylesheet">
     
+    <style type="text/css">
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  padding: 16px;
+  text-align: center;
+  background-color: #f1f1f1;
+}
+
+#labelhost {
+  font: 25px bold;
+  font-family: fantasy;
+}
+
+.sublabel {
+  font: 250% bold;
+}
+
+#labeldata {
+  font-size: 15px;
+}
+
+#slabel {
+  color: #0099cc;
+  text-shadow: 5px 2px black;
+}
+    </style>
+
   </head>
   <body>
     <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+    <nav id="muna" class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
       <div class="container">
-        <a class="navbar-brand" href="index.php"><img src="/assets/images/header.png" alt="Global" width = "250"  ></a>
+        <a class="navbar-brand" href="index.php"><img src="/assets/images/5th logo.png" alt="Global" width = "100"  ></a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         Menu
         <i class="fa fa-bars"></i>
@@ -35,14 +63,16 @@
               <a class="nav-link" href="index.php">Home</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href='/results'>Results</a>
+              <a class="nav-link" href='#purpose'>Purpose</a>
             </li>
-            
             <li class="nav-item">
               <a class="nav-link" href="contact.html">Contact</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href='/login'>Log-in</a>
+              <a class="nav-link" href='/results'>Results</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href='/login'>Vote now!</a>
             </li>
           </ul>
         </div>
@@ -50,23 +80,182 @@
     </nav>
     <!-- Page Header -->
     
-    <header class="masthead" style="background-image: url('/assets/images/chain.jpg')">
-      <div class="overlay"></div>
+    <header class="masthead" style="background-image: url('/assets/images/header image.jpg')">
       <div class="container">
         <div class="row">
-          <div class="col-lg-12 col-md-10 mx-auto">
-            <div class="site-heading">
-              <h1>Global Associations of Blockchains and Cryptocurrency</h1>
-              <span class="subheading">Be the first, we are the first</span>
-            </div>
+          <div id="headerholder" class="col-lg-12 mx-auto site-heading">
+            <h1><a id="slabel">G</a>lobal <a id="slabel">A</a>ssociations of <a id="slabel">B</a>lockchains and <a id="slabel">C</a>ryptocurrency</h1>
           </div>
         </div>
       </div>
     </header>
+
+    <div class="overlay masthead">
+      <div class="container">
+        <div class="row">
+          <div id="headerholder" class="col-lg-12 mx-auto site-heading">
+            
+            <p class="sublabel"><u>MISSION</u></p>
+            <p class="col-md-10 mx-auto">To build a strong community of experts and professionals on related emerging Blockchain Technology and Cryptocurrency, sharing news and knowledge, organizing events and promoting blockchain based solutions for start-ups, investors, organizations, public and private sector with collaborative participation of regulatory bodies.</p>
+
+            <br>
+            
+            <p class="sublabel"><u>VISION</u></p>
+            <p class="col-md-10 mx-auto">To be the front-runner Blockchain and Crypto Association that helps the society on adaptaing widely used of blockchain and cryptocurreny, forming alliances with  various industries and regulatory institutions.</p>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="masthead" style="background-image: url('/assets/images/apartment.jpg')">
+      <div class="container">
+          <div id="rower" class="row col-lg-12 mx-auto site-heading">
+
+            <div class="row">
+              <div class="column">
+                <div class="card">
+                  <img src="../assets/images/david lim.png" alt="david lim" class="col-md-12" width="100" height="150">
+                  <p id="labelhost">David S. Lim</p>
+                  <p id="labeldata" class="mx-auto">Senior VP/Pres & CEO:MY Solid Tech, Solid Group Inc.</p>
+                  <p>Philippines</p>
+                </div>
+              </div>
+
+              <div class="column">
+                <div class="card">
+                  <img src="../assets/images/mark landicho.png" alt="david lim" class="col-md-12" width="100" height="150">
+                  <p id="labelhost">Mark "Karl" Landicho</p>
+                  <p id="labeldata" class="mx-auto">CEO of 2 Companies Blockchain Speaker ICO Advisor | DLT</p>
+                  <p>Philippines</p>
+                </div>
+              </div>
+
+              <div class="column">
+                <div class="card">
+                  <img src="../assets/images/jayarr.png" alt="david lim" class="col-md-12" width="100" height="150">
+                  <p id="labelhost">Guillermo Tabligan</p>
+                  <p id="labeldata" class="mx-auto">Chief Information Officer at Digima Web Solutions, Inc</p>
+                  <p>Philippines</p>
+                </div>
+              </div>
+
+              <div class="column">
+                <div class="card">
+                  <img src="../assets/images/kanji.png" alt="david lim" class="col-md-12" width="100" height="150">
+                  <p id="labelhost">Kanji Sakae</p>
+                  <p id="lebaldata" class="mx-auto">Founder, SnowBall Capital</p>
+                  <p>JAPAN</p>
+                </div>
+              </div>
+            </div>
+
+          </div>
+      </div>
+    </div>
+
+    <div class="overlay masthead">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 mx-auto site-heading">
+            
+            <p id="purpose" class="sublabel"><u>PURPOSE</u></p>
+            <ul class="col-md-10 mx-auto" style="list-style-type:decimal;">
+              <li>To promote the use of Blockchain Technology and Cryptocurrency around globe.</li>
+              <li>To build a strong community of experts and non-experts, leaders and professionals in Blockchain and Cryptocurrency space.</li>
+              <li>To guide start-ups, companies and protect investors.</li>
+              <li>To foster an environment for innovation and adaptation of blockchain used for businesses.</li>
+              <li>To share news, knowledge and events related to Blockchain and Cryptocurrency.</li>
+              <li>To engage with experts, regulators, policymakers, lawyers and organizations making GABC the frontrunner organization in collaborating with regulatory bodies.</li>
+              <li>To form strategic partnership with major organizations or entities.</li>
+              <li>To facilitate partnerships between all kinds of organizations to move forward with blockchain.</li>
+            </ul>
+            
+          </div>
+        </div>
+      </div>
+    </div>
     
     <!-- Footer -->
     <footer>
+          <div class="masthead" style="background-image: url('/assets/images/apartment.jpg')">
       <div class="container">
+          <div class="row">
+            <div class="column">
+              <div class="card">
+                <p id="labelhost">Documents</p>
+                <ul>
+                  <li><a href="#">White Paper</a></li>
+                  <li><a href="#">Light Paper</a></li>
+                  <li><a href="#">Rival Paper</a></li>
+                  <li><a href="#">Financial Paper</a></li>
+                </ul>
+              </div>
+            </div>
+
+            <div class="column">
+              <div class="card">
+                <p id="labelhost">Follow Us</p>
+                <ul class="list-inline-item">
+                  <li>
+                    <a href="#">
+                      <span class="fa-stack fa-lg">
+                        <i class="fa fa-square fa-stack-2x"></i>
+                        <i class="fa fa-send fa-stack-1x fa-inverse"></i>
+                      </span>
+                    </a>
+                  </li>
+                  
+                  <li>
+                    <a href="#">
+                      <span class="fa-stack fa-lg">
+                        <i class="fa fa-square fa-stack-2x"></i>
+                        <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+                      </span>
+                    </a>
+                  </li>
+                  
+                  <li>
+                    <a href="#">
+                      <span class="fa-stack fa-lg">
+                        <i class="fa fa-square fa-stack-2x"></i>
+                        <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+                      </span>
+                    </a>
+                  </li>
+                  
+                  <li>
+                    <a href="#">
+                      <span class="fa-stack fa-lg">
+                        <i class="fa fa-square fa-stack-2x"></i>
+                        <i class="fa fa-youtube fa-stack-1x fa-inverse"></i>
+                      </span>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div class="column">
+              <div class="card">
+                <p id="labelhost">Get in touch with Us</p>
+                <p id="labeldata" class="mx-auto">Chief Information Officer at Digima Web Solutions, Inc</p>
+                <p>Philippines</p>
+              </div>
+            </div>
+
+            <div class="column">
+              <div class="card">
+                <p id="labelhost">or Contact us directly</p>
+                <p id="lebaldata" class="mx-auto">Founder, SnowBall Capital</p>
+                <p>JAPAN</p>
+              </div>
+            </div>
+          </div>
+    </div>
+    </div>
+
+        <br>
         <div class="row">
           <div class="col-lg-8 col-md-10 mx-auto">
             <ul class="list-inline text-center">
